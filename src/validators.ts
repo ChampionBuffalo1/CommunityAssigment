4a44356ab78164f713b62abb5998abb049d6c1c4
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-25 23:56:07 +0530

    Added all schema validators

---
+++ src/validators.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+const signUpSchema = z.object({
+  name: z.string().min(2, 'name should be at least 2 character'),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+const signInSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6),
+});
+
+const roleCreateSchema = z.object({
+  name: z.string().min(2, 'role name must be at least 2 characters'),
+});
+
+const memberCreateSchema = z.object({
+  user: z.string().min(1),
+  role: z.string().min(1),
+  community: z.string().min(1),
+});
+
+const communityCreateSchema = z.object({
+  name: z.string().min(2, 'community name should be at least 2 characters'),
+});
+
+export { signUpSchema, signInSchema, roleCreateSchema, memberCreateSchema, communityCreateSchema };
