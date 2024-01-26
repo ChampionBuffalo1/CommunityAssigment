55b853779b853c0ca47360268cc1ba040cdf1da7
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-26 23:52:56 +0530

    Added NaN check

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,8 +1,11 @@
 export const isProd: boolean = process.env.NODE_ENV === 'production';
 export const JwtSecret: string = process.env.JWT_SECRET || 'secret';
 // If the token isn't set Number will return to 0 which can be used in boolean short-circuiting
-export const API_PORT: number = Number(process.env.PORT) || 8080;
-export const maxTokenAge: number = Number(process.env.MAX_TOKEN_AGE) || 1000 * 60 * 60 * 24 * 7; // 7 days
+export const API_PORT: number =
+  (process.env.PORT && !Number.isNaN(+process.env.PORT) && parseInt(process.env.PORT)) || 3000;
+export const maxTokenAge: number =
+  (process.env.MAX_TOKEN_AGE && !Number.isNaN(+process.env.MAX_TOKEN_AGE) && parseInt(process.env.MAX_TOKEN_AGE)) ||
+  1000 * 60 * 60 * 24 * 7; // 7 days
 
 export const bcryptSaltRound = 8;
 export const RESULT_PER_PAGE = 10;
