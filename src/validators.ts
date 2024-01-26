d3d018b33c0b976bd7f08c7d6437c9dcd5e68cdc
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-26 21:33:11 +0530

    Added `required_error` for strings

--- src/validators.ts
+++ src/validators.ts
@@ -1,28 +1,62 @@
 import { z } from 'zod';
 
 const signUpSchema = z.object({
-  name: z.string().min(2, 'Name should be at least 2 characters.'),
-  email: z.string().email('Please provide a valid email address.'),
-  password: z.string().min(6, 'Password should be at least 6 characters.'),
+  name: z
+    .string({
+      required_error: 'Name should be at least 2 characters.',
+    })
+    .min(2, 'Name should be at least 2 characters.'),
+  email: z
+    .string({
+      required_error: 'Please provide a valid email address.',
+    })
+    .email('Please provide a valid email address.'),
+  password: z
+    .string({
+      required_error: 'Password should be at least 6 characters.',
+    })
+    .min(6, 'Password should be at least 6 characters.'),
 });
 
 const signInSchema = z.object({
-  email: z.string().email('Please provide a valid email address.'),
-  password: z.string().min(6),
+  email: z
+    .string({
+      required_error: 'Please provide a valid email address.',
+    })
+    .email('Please provide a valid email address.'),
+  password: z
+    .string({
+      required_error: 'Password should be at least 6 characters.',
+    })
+    .min(6, 'Password should be at least 6 characters.'),
 });
 
 const roleCreateSchema = z.object({
-  name: z.string().min(2, 'Name should be at least 2 characters.'),
+  name: z
+    .string({
+      required_error: 'Name should be at least 2 characters.',
+    })
+    .min(2, 'Name should be at least 2 characters.'),
 });
 
 const memberCreateSchema = z.object({
-  user: z.string().min(1),
-  role: z.string().min(1),
-  community: z.string().min(1),
+  user: z.string({
+    required_error: 'User ID is missing',
+  }),
+  role: z.string({
+    required_error: 'role ID is missing',
+  }),
+  community: z.string({
+    required_error: 'community ID is missing',
+  }),
 });
 
 const communityCreateSchema = z.object({
-  name: z.string().min(2, 'Name should be at least 2 characters.'),
+  name: z
+    .string({
+      required_error: 'Name should be at least 2 characters.',
+    })
+    .min(2, 'Name should be at least 2 characters.'),
 });
 
 export { signUpSchema, signInSchema, roleCreateSchema, memberCreateSchema, communityCreateSchema };
