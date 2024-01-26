d6a3aab3270a0f4979c222ea88fec30c7a669907
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-26 18:58:25 +0530

    Added re-used constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,3 +5,7 @@ export const API_PORT: number = Number(process.env.PORT) || 8080;
 export const maxTokenAge: number = Number(process.env.MAX_TOKEN_AGE) || 1000 * 60 * 60 * 24 * 7; // 7 days
 
 export const bcryptSaltRound = 8;
+export const RESULT_PER_PAGE = 10;
+export const ADMIN_ROLE_NAME = 'Community Admin',
+  MOD_ROLE_NAME = 'Community Moderator',
+  MEMBER_ROLE_NAME = 'Community Member';
