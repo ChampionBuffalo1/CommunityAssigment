07740e69e6dcb9acadafd8b1ac396718ba94dd1a
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-26 21:35:26 +0530

    Fixed jwt error handling

--- src/middleware/attachSession.ts
+++ src/middleware/attachSession.ts
@@ -1,4 +1,4 @@
-import { getJwtPayload, errorResponse } from '../utils';
+import { getJwtPayload, errorResponse, Logger } from '../utils';
 import type { NextFunction, Request, Response } from 'express';
 
 const attachSession = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -7,14 +7,16 @@ const attachSession = async (req: Request, res: Response, next: NextFunction): P
     if (jwtToken) req.payload = getJwtPayload(jwtToken);
     next();
   } catch (err) {
-    if ((err as Error).name === 'JwtError') {
-      res.status(400).json(
+    if ((err as Error).message === 'JwtError') {
+      res.status(401).json(
         errorResponse({
           code: 'INVALID_TOKEN',
           message: 'Your access token has been expired.',
         }),
       );
+      return;
     }
+    Logger.error(err);
   }
 };
 
