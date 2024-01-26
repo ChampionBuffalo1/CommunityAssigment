7ceb60b559db22efbb87961aa092907624c7ffae
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-26 21:33:49 +0530

    Fixed Clamp bug

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -8,12 +8,16 @@ type PaginatedResponse = {
 };
 
 export function getPaginatedParameters(
-  query: number,
+  query: string,
   totalRecords: number,
   resultPerPage = RESULT_PER_PAGE,
 ): PaginatedResponse {
+  let queryNum = query ? parseInt(query, 10) : 0;
+  if (Number.isNaN(queryNum) || !Number.isSafeInteger(queryNum)) {
+    queryNum = 0;
+  }
   const totalPages = Math.ceil(totalRecords / resultPerPage);
-  const currentPage = Math.min(Math.max(Number(query), 1), totalPages);
+  const currentPage = Math.max(Math.min(queryNum, totalPages), 1);
   const skip = (currentPage - 1) * resultPerPage;
   const take = resultPerPage;
   return {
