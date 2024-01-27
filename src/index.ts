e8dec8f7437d22d2c4fbda26637ee2f3e3f1cec3
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-27 09:37:34 +0530

    Added unknown route handler

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import express from 'express';
 import v1Router from './routes';
-import { Logger } from './utils';
+import { Logger, errorResponse } from './utils';
 import { API_PORT } from './Constants';
 import { attachSession } from './middleware/attachSession';
 
@@ -23,6 +23,15 @@ import { attachSession } from './middleware/attachSession';
   app.use('/v1', v1Router);
 
   app.get('/', (_, res) => res.sendStatus(200));
+  app.all('*', (req, res) =>
+    res.status(404).json(
+      errorResponse({
+        code: 'INVALID_ROUTE',
+        message: `${req.method} ${req.url} not found`,
+      }),
+    ),
+  );
+
   app.listen(API_PORT, () => Logger.info(`Server started at port ${API_PORT}`));
 
   process.on('unhandledRejection', (error: Error) =>
